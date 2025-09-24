aa0fe9c89a206b87cb720d8a53b8ffdace62d4a3
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-09-25 02:41:29 +0900

    init : project init

---
+++ src/main/java/com/cdbd/levelup/Levelup1Application.java
@@ -0,0 +1,13 @@
+package com.cdbd.levelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Levelup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Levelup1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdbd/levelup/Levelup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdbd.levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Levelup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
